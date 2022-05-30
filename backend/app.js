6081e5c9085b390e8ffcec5b0f312b99f1baa409
Dhanush1509 <s.munidhanush15@gmail.com>
2022-05-30 07:13:00 +0530

    deploy

--- backend/app.js
+++ backend/app.js
@@ -51,7 +51,10 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "http://greengrocer.vercel.app",
+    ],
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
